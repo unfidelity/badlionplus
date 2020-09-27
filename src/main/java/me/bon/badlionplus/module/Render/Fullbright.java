0112b539f32c9f7e87d94ed28439c5600772859b
yagel15637 <yagelsh2006>
2020-09-27 12:12:37 +0300

    changed FakePlayer a bit to be more consistent and fixed Fullbright that had the big retard and was flashing

--- src/main/java/me/bon/badlionplus/module/Render/Fullbright.java
+++ src/main/java/me/bon/badlionplus/module/Render/Fullbright.java
@@ -8,22 +8,36 @@ import net.minecraft.potion.PotionEffect;
 import java.util.Objects;
 
 public class Fullbright extends Module {
+	private boolean hasEffect = false;
+	private final PotionEffect effect = new PotionEffect(Objects.requireNonNull(Potion.getPotionById(16)));
+
 	public Fullbright() {
 		super("Fullbright", Category.Render);
 
+		effect.setPotionDurationMax(true);
 		this.tickDelay = 1000;
 	}
 
+	@Override
+	public void onEnable() {
+		if (mc.player == null) return;
+
+		mc.player.addPotionEffect(effect);
+		hasEffect = true;
+	}
 
 	@Override
 	public void onUpdate() {
 		if (mc.player == null) return;
 
-		mc.player.addPotionEffect(new PotionEffect(Objects.requireNonNull(Potion.getPotionById(16))));
+		if (!hasEffect) {
+			mc.player.addPotionEffect(effect);
+			hasEffect = true;
+		}
 	}
 
 	@Override
 	public void onDisable() {
-		mc.player.removeActivePotionEffect(Objects.requireNonNull(Potion.getPotionById(16)));
+		mc.player.removeActivePotionEffect(effect.getPotion());
 	}
 }
