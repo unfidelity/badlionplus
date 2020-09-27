19be4c615a186d1d36b8ee818162c9bca5ea1308
bon55 <35856276+bon55@users.noreply.github.com>
2020-09-27 14:44:28 -0400

    Update BadlionMod.java

--- src/main/java/me/bon/badlionplus/BadlionMod.java
+++ src/main/java/me/bon/badlionplus/BadlionMod.java
@@ -33,7 +33,7 @@ public class BadlionMod
 {
     public static final String MODID = "badlionplus";
     public static final String NAME = "Badlion+";
-    public static final String VERSION = "1.4.4";
+    public static final String VERSION = "1.4.5";
     
     public static CFontRenderer fontRenderer;
     
